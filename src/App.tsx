fec82f9b9559f07f06be80c771a8c658f0cb8354
Sahyadim <edson@smas.com.br>
2025-06-12 10:30:56 -0300

    Fix unused React import errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import { CompanyProvider } from './contexts/CompanyContext'
